994a344bf507933d0800e68e324b1b5cf27eaf26
Heathercat123 <antoineaurelief@icloud.com>
2024-12-12 19:26:02 -0500

    Fix static files

--- scratchrguides/settings.py
+++ scratchrguides/settings.py
@@ -67,7 +67,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'ScratchR-setup-guides/static/'
 
 STATIC_ROOT = BASE_DIR / 'static/'
 
