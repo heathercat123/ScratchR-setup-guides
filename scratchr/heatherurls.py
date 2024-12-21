98d3b43225e0757daccc84756ffebfc232fb43b0
Heathercat123 <antoineaurelief@icloud.com>
2024-12-21 11:55:27 -0500

    Fixed a typo

--- scratchr/heatherurls.py
+++ scratchr/heatherurls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
 	distill_path('xampp/', views.xampp, name='xampp', distill_file='heatherscratchr/xampp/index.html'),
-	distill_path('memcached/', views.memcached, name='memcached', distill_file='heathercratchr/memcached/index.html'),
+	distill_path('memcached/', views.memcached, name='memcached', distill_file='heatherscratchr/memcached/index.html'),
 ]
